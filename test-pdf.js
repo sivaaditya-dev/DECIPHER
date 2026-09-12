73080a59272b2e51e2d9f1a643906b1b7d259d2d
SIVA ADITYA <sivaadityashiv@gmail.com>
2026-09-12 19:11:28 +0530

    test: add test scripts for pdf-parse with DOMMatrix polyfill

---
+++ test-pdf.js
@@ -0,0 +1,8 @@
+﻿// Polyfill
+global.DOMMatrix = class DOMMatrix {
+  constructor() {
+    this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
+  }
+};
+const pdfParse = require('pdf-parse');
+console.log("Successfully required pdf-parse!");
