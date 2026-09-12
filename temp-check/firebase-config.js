6f23287698d7f298ba3a6abd2ff8af46fb759a67
SIVA ADITYA <sivaadityashiv@gmail.com>
2026-09-13 00:05:51 +0530

    feat: add Firebase configuration file

---
+++ temp-check/firebase-config.js
@@ -0,0 +1,9 @@
+// ⚠️ IMPORTANT: This file should be in your .gitignore to keep your API keys secret!
+const firebaseConfig = {
+  apiKey: "AIzaSyDKDiO4ykSitjQa-7nPmbiLO-D5EVqszUg",
+  authDomain: "vocabulary-simplifier.firebaseapp.com",
+  projectId: "vocabulary-simplifier",
+  storageBucket: "vocabulary-simplifier.firebasestorage.app",
+  messagingSenderId: "773186406632",
+  appId: "1:773186406632:web:9e53d2076de961cb9a615d"
+};
