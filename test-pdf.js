79a08574d686ad3749b363f2aecc339c92f1ee60
SIVA ADITYA <sivaadityashiv@gmail.com>
2026-09-13 16:31:10 +0530

    Cleanup: Remove test-pdf.js, test-pdf2.js, update-styles.js, integrate.js

--- test-pdf.js
+++
@@ -1,8 +0,0 @@
-﻿// Polyfill
-global.DOMMatrix = class DOMMatrix {
-  constructor() {
-    this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
-  }
-};
-const pdfParse = require('pdf-parse');
-console.log("Successfully required pdf-parse!");
